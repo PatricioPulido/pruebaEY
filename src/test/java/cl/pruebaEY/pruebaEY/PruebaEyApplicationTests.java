d81c3d19f3568ebc674ccd1fa3ae543e9738bed2
PatricioPulido <50428859+PatricioPulido@users.noreply.github.com>
2023-03-23 12:08:01 -0300

    agregacion de pruebas unitarias con junit y mockit

--- src/test/java/cl/pruebaEY/pruebaEY/PruebaEyApplicationTests.java
+++ src/test/java/cl/pruebaEY/pruebaEY/PruebaEyApplicationTests.java
@@ -3,11 +3,37 @@ package cl.pruebaEY.pruebaEY;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import cl.ey.pruebaEY.dto.PersonaDto;
+import cl.ey.pruebaEY.repository.IPersonaReptry;
+import cl.ey.pruebaEY.service.IPersonaService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.junit.Test;
 @SpringBootTest
-class PruebaEyApplicationTests {
+public class PruebaEyApplicationTests {
+	
+	@Mock
+	private IPersonaReptry personaReprty;
+	
+	@InjectMocks
+	private IPersonaService personaService;
 
 	@Test
-	void contextLoads() {
+	void crear() {
+		PersonaDto esperado= new PersonaDto("prueba");
+		Mockito.when(personaReprty.crear("prueba"))
+			.thenReturn(esperado);
+		final PersonaDto resultado =
+				personaService.crear("prueba");
+		Assertions.asserEquals(esperado.getIdPersona());
+		Asesrtions.asserEquals(esperado.getName());
+		Asesrtions.asserEquals(esperado, resultado);
+		
 	}
 
 }
